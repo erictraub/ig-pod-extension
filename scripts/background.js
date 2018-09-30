bd013b44a8dadd977ff78a2848575c2dd3875ae6
Eric Traub <est2114@columbia.edu>
2018-09-30 18:57:51 -0400

    include event emitters in BE and start using socket io

--- scripts/background.js
+++ scripts/background.js
@@ -9,6 +9,12 @@
 		ceUtilFuncs.newPostChecker();
 	});
 
+	var socket = io('http://localhost:1337');
+	
+	socket.on('new post to like', function(post) {
+	    console.log('new post to like HIT: ', post);
+	});
+
 	// START: use google alert instead of setInterval
 	// START: send out newPost via sockets
 
